d2861b379475ee034f54880c8ebd472c105bd815
Arulraj <jarulraj2@gmail.com>
2025-11-26 10:34:35 +0530

    added

--- fisability/apps.py
+++ fisability/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class FisabilityConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'fisability'
+    verbose_name = "Feasibility"
